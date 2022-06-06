196bbe551e2b0334abb0fd55d7eaaa72cfb6cd2b
Virginialaraprojects <106257931+Virginialaraprojects@users.noreply.github.com>
2022-06-06 08:50:26 -0700

    Updated project

--- js/script.js
+++ js/script.js
@@ -17,6 +17,31 @@ For assistance:
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
+const studentsPerPage= 9;
+
+function showPage(list, page){
+   const startIndex =(page * studentsPerPage)- studentsPerPage;
+   const endIndex =page *studentsPerPage;
+   const studentList = document.querySelector('.student-list');
+   studentList.innerHTML='';
+   for(let i = 0; i < list.length; i++){
+      if(i>= startIndex && i< endIndex){
+            const html =`<li class="student-item cf">
+            <div class="student-details">
+            <img class="avatar" src=${list[i].picture.large} alt="Profile Picture">
+            <h3> ${list[i].name.first} ${list[i].name.last}</h3>
+            <span class="email">${list[i].email}</span>
+            </div>
+            <div class="joined-details">
+           <span class="date">Joined ${list[i].registered.date}</span>
+           </div>
+           </li>`;
+   studentList.insertAdjacentHTML('beforeend',html);
+         
+      }  
+
+   }
+}
 
 
 
@@ -25,6 +50,34 @@ Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
 */
 
+function addPagination(list){
+   const pagButton=Math.floor(list.length /studentsPerPage);
+   const linkList=document.querySelector('.link-list');
+   linkList.innerHTML='';
+   for (let i =0; i< pagButton; i++){
+         const li=document.createElement('li');
+         const button= document.createElement('button');
+         li.appendChild(button);
+         const html= `<li>
+         <button type="button">${i +1}</button>
+         </li>`;
+
+   linkList.insertAdjacentHTML('beforeend',html);
+   li.firstElementChild.className='active';
+   }
+   linkList.addEventListener('click',(e)=>{
+         if(e.target.tagName ==='BUTTON'){
+           const activeClass =document.getElementsByClassName('active');
+           for(let i=0; i< activeClass.length; i++) 
+         e.target.className='active';
+         showPage(list, e.target.textContent);
+         }
+   })
+   
+}
+
 
 
 // Call functions
+showPage(data, 1);
+addPagination(data);
